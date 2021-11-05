9d25e268df9fe31a457029a25f5b2a04f764d9d8
chionsuk <hsuk0409@gmail.com>
2021-11-05 21:09:56 +0900

    은행과 더하기 개체, 인터페이스 추가

--- tdd-book-exam/src/main/java/com/tdd/money/Money.java
+++ tdd-book-exam/src/main/java/com/tdd/money/Money.java
@@ -1,13 +1,21 @@
 package com.tdd.money;
 
-public class Money {
+public class Money implements Expression {
 
-    protected int amount;
-    protected String currency;
+    int amount;
+    String currency;
 
     public Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
+    }
+
+    public Expression plus(Money addend) {
+        return new Sum(this, addend);
+    }
+
+    public Money reduce(String to) {
+        return this;
     }
 
     public Money times(int multiplier) {
