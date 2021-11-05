69e7b9f989b67275aa28fc70f5f99bba16673514
chionsuk <hsuk0409@gmail.com>
2021-11-05 19:04:17 +0900

    TDD 과정을 거쳐 서서히 중복 제거

--- tdd-book-exam/src/main/java/com/tdd/money/Franc.java
+++ tdd-book-exam/src/main/java/com/tdd/money/Franc.java
@@ -2,12 +2,8 @@ package com.tdd.money;
 
 public class Franc extends Money {
 
-    public Franc(int amount) {
-        this.amount = amount;
-    }
-
-    public Franc times(int multiplier) {
-        return new Franc(amount * multiplier);
+    public Franc(int amount, String currency) {
+        super(amount, currency);
     }
 
 }
--- tdd-book-exam/src/main/java/com/tdd/money/Money.java
+++ tdd-book-exam/src/main/java/com/tdd/money/Money.java
@@ -3,11 +3,36 @@ package com.tdd.money;
 public class Money {
 
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    public String currency() {
+        return this.currency;
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
 
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency);
     }
 
+    public String toString() {
+        return amount + "  " + currency;
+    }
 }
