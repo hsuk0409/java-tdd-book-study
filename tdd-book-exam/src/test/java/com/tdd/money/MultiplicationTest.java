b35d90bfb66b37972c9a69f2e1febb85da2fff73
chionsuk <hsuk0409@gmail.com>
2021-11-05 20:30:14 +0900

    Franc 개체도 제거

--- tdd-book-exam/src/test/java/com/tdd/money/MultiplicationTest.java
+++ tdd-book-exam/src/test/java/com/tdd/money/MultiplicationTest.java
@@ -14,29 +14,15 @@ public class MultiplicationTest {
     }
 
     @Test
-    public void testFrancMultiplication() {
-        Franc five = new Franc(5, "CHF");
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
     public void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(6));
-        assertEquals(new Franc(5, "CHF"), Money.franc(5));
-        assertNotEquals(new Franc(5, "CHF"), Money.franc(6));
-        assertNotEquals(new Franc(5, "CHF"), Money.dollar(5));
+        assertNotEquals(Money.franc(5), Money.dollar(5));
     }
 
     @Test
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertEquals(new Money(10, "CHF"), new Franc(10, "CHF"));
     }
 }
